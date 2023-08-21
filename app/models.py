44451ace2877329f20ad818b3ad3aee77aa61975
David Park <davidpark.kh@gmail.com>
2023-08-20 20:31:33 -0400

    cleaned up more code, added .env and draft.html to .gitignore

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from secrets import token_urlsafe
-from datetime import datetime, date
+from datetime import datetime
 
 from app import db, login
 
